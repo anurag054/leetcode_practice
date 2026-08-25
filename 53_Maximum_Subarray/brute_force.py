c14e7294d5013303ce2bba7e893aeeef952b409a
Anurag <anuragniraula0@gmail.com>
2026-08-25 21:42:55 +0545

    Create brute_force.py

---
+++ 53_Maximum_Subarray/brute_force.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = nums[0]   
+        n = len(nums)
+        for i in range(n):
+            current_sum = 0
+                for j in range(i, n):
+                    current_sum += nums[j]
+                    max_sum = max(max_sum, current_sum)
+            return max_sum
