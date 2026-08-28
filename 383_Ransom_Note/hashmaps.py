e36ce62b08399c772d3b56aef8cf3138d6565c0a
Anurag <anuragniraula0@gmail.com>
2026-08-28 23:37:15 +0545

    Create hashmaps.py

---
+++ 383_Ransom_Note/hashmaps.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counter = {}
+        for char in magazine:
+            if char in counter:
+                counter[char] += 1
+            else:
+                counter[char] = 1
+
+        for char in ransomNote:
+            if char not in counter or counter[char] == 0:
+                return False
+            counter[char] -= 1
+
+        return True
