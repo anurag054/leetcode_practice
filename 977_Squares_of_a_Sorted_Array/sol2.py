75d26ec997351ac905955679505af50d91896f3e
Anurag <anuragniraula0@gmail.com>
2026-09-06 21:53:43 +0545

    Create sol2.py

---
+++ 977_Squares_of_a_Sorted_Array/sol2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        arr = []
+        for num in nums:
+            sq = num * num
+            arr.append(sq)
+        arr.sort()
+
+        return arr
