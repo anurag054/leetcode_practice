ef1918ce00519ee4b6b4d775ce6961d0bf1930c0
Anurag <anuragniraula0@gmail.com>
2026-09-21 22:23:48 +0545

    Create plusOneManual.py

---
+++ 66_Plus_One/plusOneManual.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        i = len(digits) - 1
+        while i >= 0:
+            digits[i] += 1
+            if digits[i] < 10:
+                return digits
+            digits[i] = 0
+            i -= 1
+        return [1] + digits
